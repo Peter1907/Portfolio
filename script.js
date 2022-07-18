b92acbdbdc8bc115e63c8518e3bedf88d7d40620
Peter1907 <peter.nady.1408@gmail.com>
2022-07-18 23:54:51 +0200

    Add menu HTML, CSS & JS in the mobile version

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('test')
+const menuIcon = document.getElementById('menu-button');
+const close = document.getElementById('Close-btn');
+const menu = document.getElementById('menu-container');
+
+menuIcon.addEventListener('click', function () {
+  menu.classList.toggle('hidden');
+})
+
+close.addEventListener('click', function () {
+  menu.classList.toggle('hidden');
+})
