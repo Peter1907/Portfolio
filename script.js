f272e08527d84089ebf6fa378cec68e2488af2e8
Peter1907 <peter.nady.1408@gmail.com>
2022-07-19 00:31:44 +0200

    Modify menu behavior to be hidden after clicking an option

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const menuIcon = document.getElementById('menu-button');
 const close = document.getElementById('Close-btn');
 const menu = document.getElementById('menu-container');
+const item = document.querySelector('#menu-container ul')
 
 menuIcon.addEventListener('click', function () {
   menu.classList.toggle('hidden');
 })
 
 close.addEventListener('click', function () {
+  menu.classList.toggle('hidden');
+})
+
+item.addEventListener('click', function () {
   menu.classList.toggle('hidden');
 })
