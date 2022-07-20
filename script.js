20c7c486509723cf47b7c681a3645fe2fd13c708
Mov305 <mov3three2001@gmail.com>
2022-07-20 17:28:55 +0200

    fixed objects

--- script.js
+++ script.js
@@ -21,42 +21,63 @@ const cards = [
     header: 'Multi-Post Stories',
     list: ['css', 'html', 'bootstrap', 'Ruby'],
     img: './media/Img-Placeholder.jpg',
+    text: ' Lorem Ipsum is simply dummy text of the printing and typesetting industryLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book .It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    srcCode: 'https://github.com/Peter1907/Portfolio',
+    liveDemo: 'https://peter1907.github.io/Portfolio/',
   },
   {
     Id: 1,
     header: 'Professional Art Printing Data',
     list: ['html', 'bootstrap', 'Ruby'],
     img: './media/Snapshoot.jpg',
+    text: ' Lorem Ipsum is simply dummy text of the printing and typesetting industryLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book .It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    srcCode: 'https://github.com/Peter1907/Portfolio',
+    liveDemo: 'https://peter1907.github.io/Portfolio/',
   },
   {
     Id: 2,
     header: 'Data Dashboard Healthcare',
     list: ['html', 'bootstrap', 'Ruby'],
     img: './media/Snapshoot.jpg',
+    text: ' Lorem Ipsum is simply dummy text of the printing and typesetting industryLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book .It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    srcCode: 'https://github.com/Peter1907/Portfolio',
+    liveDemo: 'https://peter1907.github.io/Portfolio/',
   },
   {
     Id: 3,
     header: 'Website Protfolio',
     list: ['html', 'bootstrap', 'Ruby'],
     img: './media/Snapshoot.jpg',
+    text: ' Lorem Ipsum is simply dummy text of the printing and typesetting industryLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book .It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    srcCode: 'https://github.com/Peter1907/Portfolio',
+    liveDemo: 'https://peter1907.github.io/Portfolio/',
   },
   {
     Id: 4,
     header: 'Professional Art Printing Data',
     list: ['html', 'bootstrap', 'Ruby'],
     img: './media/Snapshoot.jpg',
+    text: ' Lorem Ipsum is simply dummy text of the printing and typesetting industryLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book .It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    srcCode: 'https://github.com/Peter1907/Portfolio',
+    liveDemo: 'https://peter1907.github.io/Portfolio/',
   },
   {
     Id: 5,
     header: 'Data Dashboard Healthcare',
     list: ['html', 'bootstrap', 'Ruby'],
     img: './media/Snapshoot.jpg',
+    text: ' Lorem Ipsum is simply dummy text of the printing and typesetting industryLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book .It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    srcCode: 'https://github.com/Peter1907/Portfolio',
+    liveDemo: 'https://peter1907.github.io/Portfolio/',
   },
   {
     Id: 6,
     header: 'Website Protfolio',
     list: ['html', 'bootstrap', 'Ruby'],
     img: './media/Snapshoot.jpg',
+    text: ' Lorem Ipsum is simply dummy text of the printing and typesetting industryLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book .It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    srcCode: 'https://github.com/Peter1907/Portfolio',
+    liveDemo: 'https://peter1907.github.io/Portfolio/',
   },
 ];
 
@@ -75,6 +96,11 @@ seeProjectBtn.forEach((btn, Id) => {
     const data = cards[Id];
     document.querySelector('#pop-up-head > h2').textContent = data.header;
     document.querySelector('#pop-up-body > img').src = data.img;
+    document.querySelector('#pop-up-text p').textContent = data.text;
+    document.querySelector('.buttons :nth-child(1) > a').href = data.liveDemo;
+    document.querySelector('.buttons :nth-child(2) > a').href = data.srcCode;
+
+
     items.innerHTML = '';
     data.list.forEach((ele) => {
       const item = document.createElement('li');
