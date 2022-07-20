965cf0920e6b030d9f5dbfe898d7dfdb5040630c
Peter1907 <peter.nady.1408@gmail.com>
2022-07-20 10:55:45 +0200

    Add the html, css & js for the popup card

--- script.js
+++ script.js
@@ -14,3 +14,77 @@ close.addEventListener('click', () => {
 item.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
+const cards = [
+  {
+    Id: 0,
+    header: 'Multi-Post Stories',
+    list: ['css', 'html', 'bootstrap', 'Ruby'],
+    img: './media/Img-Placeholder.jpg'
+  },
+  {
+    Id: 1,
+    header: 'Professional Art Printing Data',
+    list: ['html', 'bootstrap', 'Ruby'],
+    img: './media/Snapshoot.jpg'
+  },
+  {
+    Id: 2,
+    header: 'Data Dashboard Healthcare',
+    list: ['html', 'bootstrap', 'Ruby'],
+    img: './media/Snapshoot.jpg'
+  },{
+    Id: 3,
+    header: 'Professional Art Printing Data',
+    list: ['html', 'bootstrap', 'Ruby'],
+    img: './media/Snapshoot.jpg'
+  },{
+    Id: 4,
+    header: 'Professional Art Printing Data',
+    list: ['html', 'bootstrap', 'Ruby'],
+    img: './media/Snapshoot.jpg'
+  },{
+    Id: 5,
+    header: 'Professional Art Printing Data',
+    list: ['html', 'bootstrap', 'Ruby'],
+    img: './media/Snapshoot.jpg'
+  },{
+    Id: 6,
+    header: 'Professional Art Printing Data',
+    list: ['html', 'bootstrap', 'Ruby'],
+    img: './media/Snapshoot.jpg'
+  }
+]
+
+const popUp = document.querySelector('.pop-up');
+const closeSign = document.querySelector('#pop-up-head > span');
+const overlay = document.querySelector('.overlay');
+const items = document.querySelector('.pop-up > ul');
+closeSign.addEventListener('click', () => {
+  overlay.classList.toggle('hidden');
+  popUp.classList.toggle('hidden');
+});
+
+const seeProjectBtn = Array.from(document.querySelectorAll('.pop'));
+for (let i in seeProjectBtn) {
+  seeProjectBtn[i].addEventListener('click', () => {
+    const data = cards[i];
+    document.querySelector('#pop-up-head > h2').textContent = data.header;
+    document.querySelector('#pop-up-body > img').src = data.img;
+    items.innerHTML = '';
+    data.list.forEach(ele => {
+      let item = document.createElement('li')
+      item.textContent = ele;
+      items.appendChild(item);
+
+    })
+
+    overlay.classList.toggle('hidden');
+    popUp.classList.toggle('hidden');
+  }) 
+}
+
+overlay.addEventListener('click', () => {
+  overlay.classList.toggle('hidden');
+  popUp.classList.toggle('hidden');
+})
