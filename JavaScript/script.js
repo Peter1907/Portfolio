ee525e772823eb6564dca50252ecbba0973c66d7
Peter1907 <peter.nady.1408@gmail.com>
2022-07-24 20:10:27 +0200

    Fix ESLint errors & modifying styles

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -190,6 +190,32 @@ overlay.addEventListener('click', () => {
   document.querySelector('body').style.overflow = 'auto';
   popUp.classList.add('hidden');
   msg.style.scale = '0';
-  msg.style.left = mailIcon.offsetLeft + 8 + 'px';
-  msg.style.top = mailIcon.offsetHeight + 4 + 'px';
+  msg.style.right = 0;
+  msg.style.top = 0;
+});
+
+const mailIcon = document.querySelector('.desktop-links > li > img');
+const msg = document.querySelector('.anon-msg');
+const dismissBtn = document.querySelector('.dismiss');
+
+msg.style.position = 'absolute';
+msg.style.right = 0;
+msg.style.top = 0;
+
+mailIcon.addEventListener('click', () => {
+  msg.style.position = 'fixed';
+  msg.style.right = '50%';
+  msg.style.top = '50%';
+  msg.style.scale = 1;
+  msg.style.transform = 'translate(50%, -50%)';
+  overlay.classList.toggle('hidden');
+  document.querySelector('body').style.overflow = 'hidden';
+});
+
+dismissBtn.addEventListener('click', () => {
+  overlay.classList.toggle('hidden');
+  document.querySelector('body').style.overflow = 'auto';
+  msg.style.scale = 0;
+  msg.style.right = 0;
+  msg.style.top = 0;
 });
